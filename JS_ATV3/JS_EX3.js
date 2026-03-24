a4fb4c00cc891d4f4a483b24efa7404e45eff017
ramonbmaia <ramonmaia@outlook.com>
2026-03-23 21:27:10 -0300

    Ajustes no EX3

--- JS_ATV3/JS_EX3.js
+++ JS_ATV3/JS_EX3.js
@@ -2,4 +2,4 @@ let preco = 199.90;
 let desconto = preco * 0.25;
 let precoFinal = preco - desconto;
 
-console.log("O preço final é " + "R$ " precoFinal);
+console.log("O preço final é " + "R$ " + precoFinal);

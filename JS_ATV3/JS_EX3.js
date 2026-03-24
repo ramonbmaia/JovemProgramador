c8a7f4d13f78bb6c3b48d8a0a596175d070336ba
ramonbmaia <ramonmaia@outlook.com>
2026-03-23 21:53:29 -0300

    Alterações no EX3 de JS

--- JS_ATV3/JS_EX3.js
+++ JS_ATV3/JS_EX3.js
@@ -1,5 +1,7 @@
 let preco = 199.90;
-let desconto = preco * 0.25;
-let precoFinal = preco - desconto;
+let percentual = 25;
+let desconto = percentual / 100;
+let resultado = preco * desconto;
+let precoFinal = preco - resultado;
 
 console.log("O preço final é " + "R$ " + precoFinal);

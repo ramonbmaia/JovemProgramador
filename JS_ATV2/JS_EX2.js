c7d4494a28a857db27dc605cb04a650e1a58dc4f
ramonbmaia <ramonmaia@outlook.com>
2026-03-23 21:17:18 -0300

    Alterações no EX2

--- JS_ATV2/JS_EX2.js
+++ JS_ATV2/JS_EX2.js
@@ -1,7 +1,7 @@
 let salario = 5000;
 let contaDeLuz = 349.50;
 
-console.log (salario > contaDeLuz);
-console.log (salario < contaDeLuz);
-console.log (salario == contaDeLuz);
-console.log (salario != contaDeLuz);
+console.log(salario > contaDeLuz);
+console.log(salario < contaDeLuz);
+console.log(salario == contaDeLuz);
+console.log(salario != contaDeLuz);

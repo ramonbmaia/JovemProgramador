88e59b897652ccd6b66b3990f57bff1accd8775d
ramonbmaia <ramonmaia@outlook.com>
2026-03-23 21:07:49 -0300

    Adicionado EX2 de JS

---
+++ JS_ATV3/JS_EX1.js
@@ -0,0 +1,7 @@
+let nota1 = 6;
+let nota2 = 8.5;
+let nota3 = 5;
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+console.log("A média é " + media);
---
+++ JS_ATV3/JS_EX2.js
@@ -0,0 +1,4 @@
+let reais = 2;
+let dolares = reais * 5.23;
+
+console.log("R$ " + reais + " equivale a " + "US$ " + dolares);

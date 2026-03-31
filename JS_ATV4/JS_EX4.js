c0ce8d9e56178b2315e7ec84a86b8235781b5a31
ramonbmaia <ramonmaia@outlook.com>
2026-03-30 21:20:32 -0300

    Adicionado o EX4 da ATV4 de JS

---
+++ JS_ATV4/JS_EX4.js
@@ -0,0 +1,22 @@
+let bebida = 7;
+
+switch (bebida) {
+    case 1:
+        console.log("Água");
+        break;
+    case 2:
+        console.log("Refrigerante");
+        break;
+    case 3:
+        console.log("Chá");
+        break;
+    case 4:
+        console.log("Suco");
+        break;
+    case 5:
+        console.log("Energético");
+        break;
+    default:
+        console.log("Item não cadastrado!");
+        break;
+}
